d40825577bf7ddd7b3c5bea796b188ed7932bcd2
msiglreith <m.siglreith@gmail.com>
2020-03-28 13:27:33 +0100

    Add shader generator for windows

---
+++ shader/shader_gen.bat
@@ -0,0 +1,6 @@
+glslangValidator -V correction.comp -o .\spv\correction.comp.spv
+glslangValidator -V fft_col.comp -o .\spv\fft_col.comp.spv
+glslangValidator -V fft_row.comp -o .\spv\fft_row.comp.spv
+glslangValidator -V propagate.comp -o .\spv\propagate.comp.spv
+glslangValidator -V ocean.frag -o .\spv\ocean.frag.spv
+glslangValidator -V ocean.vert -o .\spv\ocean.vert.spv
